88ae8d86f58b3a06d94962b2dd0ef19a3228c5bc
sanglim <sanglim@kookmin.ac.kr>
2023-01-16 17:29:15 +0900

    feat. main page

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,81 @@
+import React from "react";
+import {Block, Wrap} from "./Styles";
+
+function Main() {
+    return (
+        <Wrap>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+            <Block>
+                <img src={""} />
+                <div>
+                    <div>Title</div>
+                    <span>sub title</span>
+                </div>
+            </Block>
+        </Wrap>
+    );
+}
+
+export default Main;
---
+++ src/pages/Main/Styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+  width: 100%;
+  max-width: 1140px;
+  margin: auto;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+export const Block = styled.div`
+  flex: 1 1 20%;
+`
